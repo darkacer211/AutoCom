0a509b4519b935668a99b1720b95d2c36da6269e
Atharva <atharwa.s1811@gmail.com>
2026-01-13 00:47:26 +0530

    Fix import and run email sender successfully

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
-print("MScheduler Startin ........")
+from email_bot.sender import sendemail
+
+if __name__ == "__main__":
+    sendemail(
+        to_email="atharwaiskush18@gmail.com",
+        subject="Mscheduler",
+        body="If you received this, Phase 1 is working."
+    )
